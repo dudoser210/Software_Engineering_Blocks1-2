7b7d1ac53125ab72a7c09e7aaa850d40d4a241e2
dudoser210 <dudoser210@mail.ru>
2026-09-19 23:49:44 +0300

    hhh

--- platform/helm/campuseats/templates/_helpers.tpl
+++
@@ -1,5 +0,0 @@
-{{- define "unieats.labels" -}}
-app.kubernetes.io/part-of: unieats
-app.kubernetes.io/managed-by: Helm
-{{- end -}}
-
--- platform/terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "namespaces" {
-  value = sort([for ns in kubernetes_namespace_v1.platform : ns.metadata[0].name])
-}
-
-output "service_accounts" {
-  value = sort([for sa in kubernetes_service_account_v1.services : sa.metadata[0].name])
-}
-
